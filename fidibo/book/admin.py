e6432fca1516dc6e7dd510e404628784a3a3e4c3
amirhoseinheydari <amirhoseinhydri.1381@gmail.com>
2025-04-01 12:20:26 +0330

    install DRF

--- fidibo/book/admin.py
+++ fidibo/book/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "status", "digital_version")
+    search_fields = ("title", "author__full_name")
+    prepopulated_fields = {"slug": ("title",)}
+
--- fidibo/fidibo/settings.py
+++ fidibo/fidibo/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'account.apps.AccountConfig',
     'book.apps.BookConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
